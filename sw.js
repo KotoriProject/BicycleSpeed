4104a8431942f94ce5cee958c2e94acb71e7c369
Allium-altaicum <106270331+Allium-altaicum@users.noreply.github.com>
2022-05-26 19:40:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
-const contentToCache=[
-  '/BicycleSpeed/',
+const cacheName = 'c62c-5c92-2644-e58d';
+const contentToCache = [
   '/BicycleSpeed/index2.html',
   '/BicycleSpeed/image/bicyclemove.gif',
   '/BicycleSpeed/image/bicyclestatic.png',
   '/BicycleSpeed/image/bicyclestatic2.png',
   '/BicycleSpeed/scripts/main.js',
   '/BicycleSpeed/styles/style.css'
-]
+];
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('test-store').then((cache) => cache.addAll(contentToCache)),
+    caches.open(cacheName).then((cache) => cache.addAll(contentToCache))
+  );
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
 });
 self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request.url).then((response) => response || fetch(e.request)),
-  );
+  //console.log(e.request.url);
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request.url)));
 });
