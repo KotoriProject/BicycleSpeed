a5da4ec904ad9c7015f81e90d3e15dbbc6e03b80
KotoriProject <106270562+KotoriProject@users.noreply.github.com>
2022-06-18 03:29:21 +0800

    Update main3.js

--- scripts/main3.js
+++ scripts/main3.js
@@ -9,13 +9,14 @@ let image1 = document.querySelector("#image1");
 let realSpeed = document.querySelector("#realSpeed");
 
 function switchImage(){
-    if(realSpeed.textContent==='0'){
+    if(+realSpeed.textContent===0){                         //将字符串转为数字进行判零
         image1.setAttribute('src','image/bicyclestatic.png');
     }
     else{
         image1.setAttribute('src',"image/bicyclemove.gif");
     }
 }
+switchImage();
 
 function lightAndSound(){
     document.body.style.backgroundColor="red";
@@ -27,42 +28,36 @@ function offLightAndSound(){
 }
 
 function setSpeed() {
-    let speed = prompt("请输入速度阈值。");
+    let speed = prompt("请输入速度阈值（0~255）。");
     if (speed === null) return 0;
     let x = +speed;
     if(!(Number.isFinite(x))){
         alert("您输入的值含有非法字符！");
         setSpeed();
     }
-    if (x <= 0 || x>255 || !speed) {
+    if (x <= 0 || x>255 || !speed ) {
         alert("您输入的速度阈值太小或太大！");
         setSpeed();
     } else {
         localStorage.setItem("setSpeed", x);
+        valueSpeed.textContent = localStorage.getItem("setSpeed");
         sendSpeed();
-        //valueSpeed.textContent = localStorage.getItem("setSpeed");
-
     }
 }
 
-/*function sendSpeed(){
-    const uint8arr=Uint8Array.of(localStorage.getItem("setSpeed"));
-    bicycle.writeSpeed(uint8arr);
-}
-*/
+
 function sendSpeed() {
     const uint8arr = Uint8Array.of(localStorage.getItem("setSpeed"));
     bicycle.writeSpeed(uint8arr);
 }
+
 myButton.onclick = function () {
     setSpeed();
 };
-/*blueButton.onclick=function(){
-}*/
 
 discon.onclick = function () {
     bicycle.disconnect();
-    realSpeed.textContent = 0;
+    realSpeed.textContent = '0.00';
     offLightAndSound();
     switchImage();
 };
@@ -120,13 +115,13 @@ class Bicycle {
             .then((characteristic) => {
                 characteristic.addEventListener("characteristicvaluechanged", (e) => {
                     let data = e.target.value;
-                    let tSpeed=data.getfloat32(0,true);//读取速度阈值数据
-                    let rSpeed=data.getfloat32(1,true);//读取当前速度数据
+                    let tSpeed=data.getFloat32(0,true).toFixed(0);//读取速度阈值数据
+                    let rSpeed=data.getFloat32(4,true).toFixed(2);//读取当前速度数据
                     localStorage.setItem("setSpeed", tSpeed);
                     valueSpeed.textContent = localStorage.getItem("setSpeed");
                     realSpeed.textContent = rSpeed;
                     switchImage();
-                    if(rSpeed >= tSpeed){
+                    if(+rSpeed >= +tSpeed){
                         lightAndSound();
                     }
                     else{
@@ -160,8 +155,8 @@ blueButton.addEventListener("click", (event) => {
         .then((_) => {
             /*bicycle.readSpeed().then(value =>{
               console.log(value.getUint8());*/
+            sendSpeed();
             bicycle.startNotifications();
-            
         }
         )
         .catch((error) => {
